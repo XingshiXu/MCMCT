75172d3a58e14f70f18a6d6f21624174963f575e
DayDayUp <111053588+XingshiXu@users.noreply.github.com>
2025-06-16 10:48:08 +0800

    Delete Code_MCMCT directory

--- Code_MCMCT/4_single-camera_multi-cow_tracking/boxmot-mater/boxmot/trackers/botsort/__init__.py
+++
@@ -1,2 +0,0 @@
-# Mikel Broström 🔥 Yolo Tracking 🧾 AGPL-3.0 license
-# This Code was Improved by Hings Hsu
--- Code_MCMCT/4_single-camera_multi-cow_tracking/boxmot-mater/boxmot/trackers/deepocsort/__init__.py
+++
@@ -1,2 +0,0 @@
-# Mikel Broström 🔥 Yolo Tracking 🧾 AGPL-3.0 license
-# Improved by HsuHing(许兴时)
